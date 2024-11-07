430e3065bced0a2dd894a8d671e0439996770e36
James Gore <73591234+jgore077@users.noreply.github.com>
2024-11-07 14:29:59 -0500

    add prompts

--- Llama.py
+++ Llama.py
@@ -1,5 +1,11 @@
 # A class used to interface with the Llama model provided on our servers
 
+# Audrey
+PROMPT_1=""
+
+# Jimmy
+PROMPT_2=""
+
 class Llama():
     def __init__(self) -> None:
         pass
