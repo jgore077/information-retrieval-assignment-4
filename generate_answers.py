f0dc6b0950d8b54b1d662245dd255c8a7b0e3c90
James Gore <james.gore@maine.edu>
2024-11-12 11:12:08 -0500

    add expanded query files

--- generate_answers.py
+++ generate_answers.py
@@ -8,6 +8,7 @@ KEY="Title"
 ID="Id"
 DATA_DIR="data/"
 topics_path=sys.argv[1]
+prefix=sys.argv[2]
 
 prompt_1_dict={}
 prompt_2_dict={}
@@ -19,10 +20,12 @@ with open(topics_path,'r',encoding='utf-8') as topics_file:
 # Assign generated text to new dictionary
 for topic in tqdm(topics_dict,desc="Generating answers for PROMPT_1"):
     text=topic[KEY]
-    prompt_1_dict[topic[ID]]=llm.generate(text)
+    answer=llm.generate(text)
+    print(answer)
+    prompt_1_dict[topic[ID]]=answer
 
 # Write file to data directory
-with open(f'{DATA_DIR}prompt1.json','w') as prompt_1_file:
+with open(f'{DATA_DIR}{prefix}_prompt1.json','w') as prompt_1_file:
     prompt_1_file.write(json.dumps(prompt_1_dict,indent=4))
 
 # For prompt 2   
@@ -30,8 +33,10 @@ llm=Llama(PROMPT_2)
 
 for topic in tqdm(topics_dict,desc="Generating answers for PROMPT_2"):
     text=topic[KEY]
-    prompt_2_dict[topic[ID]]=llm.generate(text)
+    answer=llm.generate(text)
+    print(answer)
+    prompt_2_dict[topic[ID]]=answer
     
-with open(f'{DATA_DIR}prompt2.json','w') as prompt_2_file:
+with open(f'{DATA_DIR}{prefix}_prompt2.json','w') as prompt_2_file:
     prompt_2_file.write(json.dumps(prompt_2_dict,indent=4))
     
